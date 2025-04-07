17e2b9980a46c19259fd7b341cf869c45e9015c3
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2025-04-07 23:04:14 +0300

    Use SPDX license tag and update copyright
    Replace the license boilerplate text with an SPDX tag, and update the
    copyright notice to cover the last 15 years of development.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Reviewed-by: Sakari Ailus <sakari.ailus@linux.intel.com>

--- yavta.c
+++ yavta.c
@@ -1,20 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * yavta --  Yet Another V4L2 Test Application
  *
- * Copyright (C) 2005-2010 Laurent Pinchart <laurent.pinchart@ideasonboard.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
+ * Copyright (C) 2005-2025 Laurent Pinchart <laurent.pinchart@ideasonboard.com>
  */
 
 #define __STDC_FORMAT_MACROS
